47718e7693f7977dbd80bd64651b1db64f863fb4
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-02 15:16:17 -0400

    Added ms (21,600,000 = 6 hr)

--- bumpbot.js
+++ bumpbot.js
@@ -5,22 +5,9 @@ const client = new Discord.Client();
 client.on("ready", () => {
   console.log("I'M UP!!! sheesh...");
 });
-
+var intervalID = scope.setInterval(code, 21600000);
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-   if (!client.commands.has(command)) return;
-
-try {
-    client.commands.get(command).execute(message, args);
-}
-catch (error) {
-    console.error(error);
-    message.reply('there was an error trying to execute that command!');
-}
+   
 });
 
 client.login(token);
