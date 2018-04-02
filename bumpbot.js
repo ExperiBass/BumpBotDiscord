23152abad503e87779dca06dd593adc6761d0eb6
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-02 15:17:47 -0400

    Update bumpbot.js

--- bumpbot.js
+++ bumpbot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 client.on("ready", () => {
   console.log("I'M UP!!! sheesh...");
 });
-var intervalID = scope.setInterval(code, 21600000);
+var intervalID = scope.setInterval(message.channel.send(`dlm!bump`), 21600000);
 client.on('message', message => {
    
 });
