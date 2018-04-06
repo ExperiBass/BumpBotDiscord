4dff7714d9a5344f5c5f31702ca596adff552bbe
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-06 04:12:21 -0400

    Update bumpbot.js

--- bumpbot.js
+++ bumpbot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { prefix, token } = require('./config.json');
+const { prefix, token, channel } = require('./config.json');
 const client = new Discord.Client();
 
 client.on("ready", () => {
